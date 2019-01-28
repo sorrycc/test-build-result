6ca3d1ba01f7fe6c7ee166659982248490067bb5
william <geek_go@163.com>
2019-01-28 18:15:40 +0800

    fix:windows EOL

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const { readdirSync, readFileSync, existsSync } = require('fs');
 const { join } = require('path');
 const glob = require('glob');
 
+// 在windows环境下，很多工具都会把换行符lf自动改成crlf，修改了一下。
+// https://github.com/cssmagic/blog/issues/22
+const isWindows =
+  typeof process !== 'undefined' && process.platform === 'win32';
+const winEOL = content => {
+  if (typeof content !== 'string') {
+    return content;
+  }
+  return isWindows ? content.replace(/\r/g, '') : content;
+};
+
 function noop(content) {
   return content;
 }
@@ -19,7 +30,7 @@ function assertBuildResult({ cwd, replaceContent = noop }) {
   actualFiles.forEach(file => {
     const actualFile = readFileSync(join(actualDir, file), 'utf-8');
     const expectFile = readFileSync(join(expectDir, file), 'utf-8');
-    expect(replaceContent(actualFile).trim()).toEqual(expectFile.trim());
+    expect(winEOL(replaceContent(actualFile).trim())).toEqual(winEOL(expectFile.trim()));
   });
 }
 
